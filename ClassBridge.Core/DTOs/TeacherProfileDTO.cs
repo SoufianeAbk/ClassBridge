7c9b0201ff6112f960855aeb5024870758f77650
SoufianeAbk <229384282+sso-vbk@users.noreply.github.com>
2025-12-23 20:01:16 +0100

    Data Transfer Objects Authenticatie, Profielen & Creatie.

---
+++ ClassBridge.Core/DTOs/TeacherProfileDTO.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassBridge.Core.DTOs
+{
+    internal class TeacherProfileDTO
+    {
+    }
+}
