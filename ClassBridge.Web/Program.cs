d232ea9080c97a9d79b9699504b73e51c8a4a26d
SoufianeAbk <229384282+sso-vbk@users.noreply.github.com>
2025-12-25 18:42:08 +0100

    Styling Update.

--- ClassBridge.Web/Program.cs
+++ ClassBridge.Web/Program.cs
@@ -1,7 +1,35 @@
+using ClassBridge.Core.Interfaces;
+using ClassBridge.Web.Data;
+using ClassBridge.Web.Services;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+// Add DbContext
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection") ??
+        "Server=(localdb)\\mssqllocaldb;Database=ClassBridgeDb;Trusted_Connection=true;"));
+
+// Add Services (Dependency Injection)
+builder.Services.AddScoped<AuthService>();
+builder.Services.AddScoped<IParentService, ParentService>();
+builder.Services.AddScoped<ITeacherService, TeacherService>();
+
+// Add Authentication
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(options =>
+    {
+        options.LoginPath = "/Account/Login";
+        options.LogoutPath = "/Account/Logout";
+        options.AccessDeniedPath = "/Account/AccessDenied";
+    });
+
+// Add Authorization
+builder.Services.AddAuthorization();
 
 var app = builder.Build();
 
@@ -16,6 +44,7 @@ if (!app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapStaticAssets();
@@ -24,6 +53,5 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}")
     .WithStaticAssets();
-
 
 app.Run();
