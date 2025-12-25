18a4b8619f7f6787f9e6898122cf56c337ffa2da
SoufianeAbk <229384282+sso-vbk@users.noreply.github.com>
2025-12-25 15:30:42 +0100

    Interface/Service Voor Leraar/Ouder Operaties.

--- ClassBridge.Core/Interfaces/IParentService.cs
+++ ClassBridge.Core/Interfaces/IParentService.cs
@@ -1,12 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using ClassBridge.Core.Entities;
 
 namespace ClassBridge.Core.Interfaces
 {
-    internal class IParentService
+    public interface IParentService
     {
+        // Parent CRUD operations
+        Task<Parent?> GetParentByIdAsync(int id);
+        Task<Parent?> GetParentByUserIdAsync(int userId);
+        Task<IEnumerable<Parent>> GetAllParentsAsync();
+        Task<Parent> CreateParentAsync(Parent parent);
+        Task<Parent> UpdateParentAsync(Parent parent);
+        Task<bool> DeleteParentAsync(int id);
+
+        // Child management
+        Task<IEnumerable<Child>> GetChildrenByParentIdAsync(int parentId);
+        Task<Child> AddChildAsync(Child child);
+        Task<Child> UpdateChildAsync(Child child);
+        Task<bool> RemoveChildAsync(int childId);
+        Task<Child?> GetChildByIdAsync(int childId);
+
+        // Search
+        Task<IEnumerable<Parent>> SearchParentsByNameAsync(string name);
+        Task<IEnumerable<Parent>> SearchParentsByCityAsync(string city);
     }
 }
--- ClassBridge.Core/Interfaces/ITeacherService.cs
+++ ClassBridge.Core/Interfaces/ITeacherService.cs
@@ -1,12 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using ClassBridge.Core.Entities;
+using ClassBridge.Core.Enums;
 
 namespace ClassBridge.Core.Interfaces
 {
-    internal class ITeacherService
+    public interface ITeacherService
     {
+        // Teacher CRUD operations
+        Task<Teacher?> GetTeacherByIdAsync(int id);
+        Task<Teacher?> GetTeacherByUserIdAsync(int userId);
+        Task<IEnumerable<Teacher>> GetAllTeachersAsync();
+        Task<Teacher> CreateTeacherAsync(Teacher teacher);
+        Task<Teacher> UpdateTeacherAsync(Teacher teacher);
+        Task<bool> DeleteTeacherAsync(int id);
+
+        // Search and filter
+        Task<IEnumerable<Teacher>> SearchTeachersBySubjectAsync(Subject subject);
+        Task<IEnumerable<Teacher>> SearchTeachersByNameAsync(string name);
+        Task<IEnumerable<Teacher>> GetAvailableTeachersAsync();
+
+        // Teacher strengths
+        Task<IEnumerable<TeacherStrength>> GetTeacherStrengthsAsync(int teacherId);
+        Task<TeacherStrength> AddTeacherStrengthAsync(TeacherStrength strength);
+        Task<bool> RemoveTeacherStrengthAsync(int strengthId);
+
+        // Teacher availability
+        Task<IEnumerable<TeacherAvailability>> GetTeacherAvailabilityAsync(int teacherId);
+        Task<TeacherAvailability> AddAvailabilityAsync(TeacherAvailability availability);
+        Task<bool> RemoveAvailabilityAsync(int availabilityId);
+        Task<bool> UpdateAvailabilityAsync(TeacherAvailability availability);
     }
 }
